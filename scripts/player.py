9b1a8cc228749190a53ec28990b8f1928e233556
Ian Schaafstra <ianschaafstra@hotmail.com>
2026-01-08 13:43:15 +0100

    added the frog sprite to player.py

--- scripts/player.py
+++ scripts/player.py
@@ -7,7 +7,8 @@ pygame.init()
 class Player:
     def __init__(self, location: pygame.Vector2) -> None:
         self.pos = location
-        self.rect = pygame.Rect(self.pos.x, self.pos.y, 64, 64)
+        self.sprite = pygame.image.load("../assets/Frog.png")
+        self.rect = self.sprite.get_rect(topleft=self.pos)
 
     def update(self):
         keys = pygame.key.get_just_pressed()  # input handling happens within the update function for now. We could implement an input handling script later and pass it as a parameter.
@@ -24,4 +25,5 @@ class Player:
         self.rect = pygame.Rect(self.pos.x, self.pos.y, 64, 64)
 
     def draw(self, screen: pygame.Surface):
-        pygame.draw.rect(screen, "green", self.rect)
+        # pygame.draw.rect(screen, "green", self.rect)
+        screen.blit(self.sprite, self.pos)
