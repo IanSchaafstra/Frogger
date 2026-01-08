7ccc73491c3d4dc84db3b24f037256645cf3db01
Reindert <reindert.overvest@itvitaelearning.nl>
2026-01-08 14:52:40 +0100

    added textures to the cars

--- scripts/car.py
+++ scripts/car.py
@@ -1,15 +1,27 @@
 import pygame
+import os
 from pygame import Rect, Color
 
 class Car:
-    def __init__(self, x: int, y: int, direction: int):
-        
-        self.hitbox = Rect(x, y, 64, 64)
-        #direction 1 for right  -1 for left
-        self.direction = direction
+    def __init__(self, x: int, y: int, driving_rightwards: bool):
+        if os.name == "posix":
+            self.path = os.path.join("..", "assets", "Car.png")
+        elif os.name == "nt":
+            self.path = os.path.join("assets", "Car.png")
+        else:
+            print("Error with asset loading, please report")
+            sys.exit()
+        self.sprite = pygame.image.load(self.path)
+        if driving_rightwards:
+            self.sprite = pygame.transform.flip(self.sprite, True, False)
+        self.hitbox = Rect(x, y, 128, 64)
+        self.driving_rightwards = driving_rightwards
 
     def update(self, dt: float):
-        self.hitbox.x += int(0.2 * self.direction * dt)
+        movement = int(0.2 * dt)
+        if not self.driving_rightwards:
+            movement *= -1
+        self.hitbox.x += movement
 
     def draw(self, surface: pygame.Surface):
-        pygame.draw.rect(surface, Color(255, 0, 0), self.hitbox)
+        surface.blit(self.sprite, self.hitbox)
