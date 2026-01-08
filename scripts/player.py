c0d85de0a7178f03dbe9678ff3b84f0749a75287
Ian Schaafstra <ianschaafstra@hotmail.com>
2026-01-08 14:32:39 +0100

    Made it so that the player faces the direction it moves in

--- scripts/player.py
+++ scripts/player.py
@@ -21,20 +21,25 @@ class Player:
         # elif os.name == "nt":
         #    self.sprite = pygame.image.load("..\\assets\\Frog.png")
         self.sprite = pygame.image.load(self.path)
+        self.rot_sprite = self.sprite
         self.rect = self.sprite.get_rect(topleft=self.pos)
 
     def update(self):
         keys = pygame.key.get_just_pressed()  # input handling happens within the update function for now. We could implement an input handling script later and pass it as a parameter.
         if keys[pygame.K_w]:
             self.pos.y -= 64
+            self.rot_sprite = pygame.transform.rotate(self.sprite, 0)
         if keys[pygame.K_s]:
             self.pos.y += 64
+            self.rot_sprite = pygame.transform.rotate(self.sprite, 180)
         if keys[pygame.K_a]:
+            self.rot_sprite = pygame.transform.rotate(self.sprite, 90)
             self.pos.x -= 64
         if keys[pygame.K_d]:
             self.pos.x += 64
+            self.rot_sprite = pygame.transform.rotate(self.sprite, -90)
         self.rect = pygame.Rect(self.pos.x, self.pos.y, 64, 64)
 
     def draw(self, screen: pygame.Surface):
         # pygame.draw.rect(screen, "green", self.rect)
-        screen.blit(self.sprite, self.pos)
+        screen.blit(self.rot_sprite, self.pos)
