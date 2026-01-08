9b79b3937b86fbb8fd813c8d7df34c00c8c80231
Reindert <reindert.overvest@itvitaelearning.nl>
2026-01-08 11:55:15 +0100

    made the car able to go to the right

--- scripts/car.py
+++ scripts/car.py
@@ -2,11 +2,14 @@ import pygame
 from pygame import Rect, Color
 
 class Car:
-    def __init__(self, x: int, y: int):
+    def __init__(self, x: int, y: int, direction: int):
+        
         self.hitbox = Rect(x, y, 64, 64)
+        #direction 1 for right  -1 for left
+        self.direction = direction
 
     def update(self, dt: float):
-        self.hitbox.x -= 0.2 * dt
+        self.hitbox.x += 0.2 * self.direction * dt
 
     def draw(self, surface: pygame.Surface):
         pygame.draw.rect(surface, Color(255, 0, 0), self.hitbox)
