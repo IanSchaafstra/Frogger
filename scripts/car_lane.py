1f04c7b0ce158957a33b07d7db5c7413a3dc490e
Reindert <reindert.overvest@itvitaelearning.nl>
2026-01-15 10:49:37 +0100

    made car lanes be able to go different speeds

--- scripts/car_lane.py
+++ scripts/car_lane.py
@@ -3,11 +3,14 @@ import random
 from car import Car
 
 class CarLane:
-    def __init__(self, y: int, driving_rightwards: bool):
+    speed = 200.0
+
+    def __init__(self, y: int, driving_rightwards: bool, speed_multiplier: float = 1):
         self.y: int = y
         self.driving_rightwards: bool = driving_rightwards
         self.cars: list[Car] = []
         self.time_until_next_car: float = 0.0
+        self.speed_multiplier = speed_multiplier
 
     def update(self, dt: float):
         # update cars
@@ -24,9 +27,9 @@ class CarLane:
         self.time_until_next_car -= dt
         if self.time_until_next_car <= 0:
             if self.driving_rightwards:
-                self.cars.append(Car(-128, self.y, True))
+                self.cars.append(Car(-128, self.y, True, CarLane.speed * self.speed_multiplier))
             else:
-                self.cars.append(Car(1280, self.y, False))
+                self.cars.append(Car(1280, self.y, False, CarLane.speed * self.speed_multiplier))
             self.time_until_next_car += random.randint(1000, 4000) / 1000
 
     def draw(self, surface: pygame.Surface):
