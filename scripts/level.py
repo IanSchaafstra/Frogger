85e4d8abbd847f4ed4bfcf4bfed9dce115930415
Ian Schaafstra <ianschaafstra@hotmail.com>
2026-01-15 11:02:06 +0100

    imported player in level.py and made the type of player in __init__ as Player. Also removed 'None' as default value.

--- scripts/level.py
+++ scripts/level.py
@@ -1,24 +1,26 @@
 import pygame
 import sys
 from car_lane import CarLane
+from player import Player
+
 
 class Level:
-    def __init__(self, player=None, screen_size=(1280, 960)):
+    def __init__(self, player: Player, screen_size=(1280, 960)):
         self.player = player
         self.screen_width, self.screen_height = screen_size
 
         self.background_color = (135, 206, 235)  # sky blue
-        self.startfinish_color = (0, 255, 0)     # green
-        self.platform_color = (100, 100, 100)    # grey
+        self.startfinish_color = (0, 255, 0)  # green
+        self.platform_color = (100, 100, 100)  # grey
 
         self.platforms = []
         self.platforms.append(pygame.Rect(0, 400, self.screen_width, 50))
 
         self.finish_zone = pygame.Rect(0, 0, self.screen_width, 50)
-        self.start_zone = pygame.Rect(0, self.screen_height - 50, self.screen_width, 50)        
+        self.start_zone = pygame.Rect(0, self.screen_height - 50, self.screen_width, 50)
 
         self.car_lanes: list[CarLane] = [CarLane(384, True), CarLane(448, False)]
-    
+
         self._on_start_last_frame = False
         self._on_finish_last_frame = False
 
@@ -41,7 +43,10 @@ class Level:
 
     def check_collisions(self):
         for car_lane in self.car_lanes:
-            if self.player.rect.collidelist([car.hitbox for car in car_lane.cars]) != -1:
+            if (
+                self.player.rect.collidelist([car.hitbox for car in car_lane.cars])
+                != -1
+            ):
                 # hit car code
                 sys.exit()
 
@@ -49,17 +54,20 @@ class Level:
         screen.fill(self.background_color)
 
         # Start/Finish zones
-        pygame.draw.rect(screen, self.startfinish_color, self.finish_zone) 
-        pygame.draw.rect(screen, self.startfinish_color, self.start_zone)  
+        pygame.draw.rect(screen, self.startfinish_color, self.finish_zone)
+        pygame.draw.rect(screen, self.startfinish_color, self.start_zone)
 
         # Platforms
         for plat in self.platforms:
             pygame.draw.rect(screen, self.platform_color, plat)
-        
+
         # Cars
         for car_lane in self.car_lanes:
             car_lane.draw(screen)
 
     def reset_player(self):
         self.player.pos.update(self.screen_width // 2, self.screen_height - 100)
-        self.player.rect.topleft = (self.player.pos.x, self.player.pos.y)
+        self.player.rect.topleft = (
+            self.player.pos.x,
+            self.player.pos.y,
+        )  # this line is redundant. It doesn't seem to be causing issues, but it gives an error in my code editor.
