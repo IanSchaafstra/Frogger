5cc701ccf336a773113951bc806418448ced0eca
Ian Schaafstra <ianschaafstra@hotmail.com>
2026-02-06 16:44:34 +0100

    added HITBOX_SHRINK_AMOUNT to constants.py to use with player.py

--- constants.py
+++ constants.py
@@ -6,3 +6,5 @@ DEVELOPMENT_MODE = True
 CAR_BASE_SPEED = 200.0
 MIN_TIME_BETWEEN_CARS = 1000  # in milliseconds
 MAX_TIME_BETWEEN_CARS = 4000  # in milliseconds
+
+HITBOX_SHRINK_AMOUNT = 6  # NOTE: the amount of pixels the player hitbox shrinks
