396842692f74ae0b3910c34c2b07ca71e162ea07
Ian Schaafstra <ianschaafstra@hotmail.com>
2026-02-06 13:43:17 +0100

    cleaned up existing method and added `get_input_hold()`

--- inputhandling.py
+++ inputhandling.py
@@ -1,13 +1,12 @@
 import pygame
-from pygame.key import ScancodeWrapper
 
 
 pygame.init()
 
 
 class Input_Handling:
-    def get_input(
-        self,
-    ) -> ScancodeWrapper:  # get_just_pressed() returns a ScancodeWrapper
-        keys = pygame.key.get_just_pressed()
-        return keys
+    def get_input_tap(self):
+        return pygame.key.get_just_pressed()
+
+    def get_input_hold(self):
+        return pygame.key.get_pressed()
